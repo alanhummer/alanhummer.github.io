4b9a3a1c5a8a94e57fd404ee92704e0dc3a6a2b1
alanhummer <62815820+alanhummer@users.noreply.github.com>
2023-07-20 20:59:58 -0500

    sdf

--- RetirementWizard/RetirementCalculator.js
+++ RetirementWizard/RetirementCalculator.js
@@ -11,13 +11,13 @@ function onDOMContentLoaded() {
     var outputTable = "<table border='0' cellpadding='0' cellspacing='0' width='100%'>_RETIREMENTFIELDS_</table>";
     var outputRows = "";
 
-    gFieldArray.push(new RetirementField("yearBorn", "2001", "YEAR BORN"));
-    gFieldArray.push(new RetirementField("yearRetire", "2001", "YEAR RETIRE"));
-    gFieldArray.push(new RetirementField("yearSocSec", "2001", "YEAR SOCSEC"));
-    gFieldArray.push(new RetirementField("yearMedicare", "2001", "YEAR MEDICARE"));
-    gFieldArray.push(new RetirementField("yearDie", "2001", "YEAR DIE"));
-    gFieldArray.push(new RetirementField("rothIRAAl", "2001", "IRA BAL"));
-    gFieldArray.push(new RetirementField("rothIRAAl-return", "2001", "IRA RETURN"));
+    gFieldArray.push(new RetirementField("yearBorn", "2001", "YEAR BORN", "year"));
+    gFieldArray.push(new RetirementField("yearRetire", "2001", "YEAR RETIRE", "year"));
+    gFieldArray.push(new RetirementField("yearSocSec", "2001", "YEAR SOCSEC", "year"));
+    gFieldArray.push(new RetirementField("yearMedicare", "2001", "YEAR MEDICARE", "year"));
+    gFieldArray.push(new RetirementField("yearDie", "2001", "YEAR DIE", "year"));
+    gFieldArray.push(new RetirementField("rothIRAAl", "2001", "IRA BAL", "money", "start", "rothIRAAl-return"));
+    gFieldArray.push(new RetirementField("rothIRAAl-return", "2001", "IRA RETURN", "rate"));
 
     //Build our display
     gFieldArray.forEach((fieldObject) => {
@@ -42,6 +42,12 @@ function doCalc() {
     var myValue;
     var i = 0;
     var blnBogus = false;
+    var startAmount = 0;
+    var addAmount = 0;
+    var subtractAmount = 0;
+    var endAmount = 0;
+    var lFieldArray;
+
     resultReport = "";
 
     document.getElementById("errormessage").innerHTML = "&nbsp;";
@@ -54,6 +60,7 @@ function doCalc() {
         else {
             fieldObject.fieldValue = document.getElementById(fieldObject.fieldName).value;
             fieldObject.setDataValue();
+            console.log("FIELD VALUE SET " + fieldObject.fieldName + " = " + fieldObject.fieldValue);
         }
     });
 
@@ -62,11 +69,17 @@ function doCalc() {
         return;
     }
 
-    //And write out results
-    
+    //Data is good, lets rip thru it and apply our time algorithm..since updating, make a copy
+    lFieldArray = gFieldArray;
+
     resultReport = "<table class='output-report' border='1px' width='100%' cellpadding='10' cellspacing='10'><tr><td align='center'>YEAR</td><td align='center'>START</td><td align='center'>ADD</td><td align='center'>SUBT</td><td align='center'>END</td></tr>"
-    for (let i = document.getElementById("yearRetire").value; i < document.getElementById("yearDie").value; i++) {
-        resultReport = resultReport + "<tr><td align='center' width='20%'>" + i + "</td><td width='20%' align='right'>$5.5M&nbsp;</td><td width='20%' align='right'>$100K</td><td width='20%' align='right'>$150K</td><td width='20%' align='right'>$5.5M</td></tr>";
+    for (let i = getValue(lFieldArray, "yearRetire"); i < getValue(lFieldArray, "yearDie"); i++) {
+        startAmount = calcStartAmounts(lFieldArray);
+        addAmount = calcAddAmounts(lFieldArray);
+        subtractAmount = calcSubtractAmounts(lFieldArray);
+        endAmount = calcEndAmounts(lFieldArray);
+
+        resultReport = resultReport + "<tr><td align='center' width='20%'>" + i + "</td><td width='20%' align='right'>" + startAmount + "&nbsp;</td><td width='20%' align='right'>" + addAmount + "</td><td width='20%' align='right'>" + subtractAmount + "</td><td width='20%' align='right'>" + endAmount + "</td></tr>";
     }
     resultReport = resultReport + "</table>";
 
@@ -93,18 +106,110 @@ function showPageView(inputView) {
     document.getElementById(inputView).style.display = "block";
 }
 
+/****************
+calcStartAmounts
+****************/
+function calcStartAmounts(inputArray) {
+
+    myValue = 0;
+
+    //Build our amount set
+    inputArray.forEach((fieldObject) => {
+        if (fieldObject.fieldType == "money" && fieldObject.startOrEnd == "start") {
+            myValue = myValue + fieldObject.fieldValue;
+        }
+    });
+
+    return myValue;
+
+}
+
+/****************
+calcAddAmounts
+****************/
+function calcAddAmounts(inputArray) {
+
+    myValue = 55;
+
+    return myValue;
+
+}
+
+/****************
+calcSubtractAmounts
+****************/
+function calcSubtractAmounts(inputArray) {
+
+    myValue = 33;
+
+    return myValue;
+
+}
+
+/****************
+calcEndAmounts
+****************/
+function calcEndAmounts(inputArray) {
+
+    myValue = 0;
+    myRate = 0;
+
+    //Build our amount set
+    inputArray.forEach((fieldObject) => {
+        if (fieldObject.fieldType == "money" && fieldObject.startOrEnd == "start") {
+            myRate = getValue(inputArray, fieldObject.rateField)
+            myValue = Number(myValue) + Number(fieldObject.fieldValue) + (Number(fieldObject.fieldValue) * Number(myRate)/100);
+        }
+    });
+
+    return myValue;
+
+}
+
+/****************
+getValue
+****************/
+function getValue(inputArray, inputFieldName) {
+
+    myValue = "";
+
+    //Build our display
+    inputArray.forEach((fieldObject) => {
+        if (fieldObject.fieldName == inputFieldName) {
+            if (!isNaN(fieldObject.fieldValue)) {
+                myValue = Number(fieldObject.fieldValue);
+            }
+            else {
+                myValue = fieldObject.fieldValue;
+            }
+        }
+    });
+
+    return myValue;
+
+}
+
 
 /****************
 Retirement Field Object
 ****************/
 class RetirementField {
 
-    constructor(fieldName, fieldValue, fieldDescription) {
+    constructor(fieldName, fieldValue, fieldDescription, fieldType = "", startOrEnd = "", rateField = "") {
         this.fieldName = fieldName;
-        this.fieldValue = fieldValue;
+        if (!isNaN(fieldValue)) {
+            this.fieldValue = Number(fieldValue);
+        }
+        else {
+            this.fieldValue = fieldValue;
+        }
+
         this.fieldDescription = fieldDescription;
+        this.fieldType = fieldType;
+        this.startOrEnd = startOrEnd;
+        this.rateField = rateField;
         this.initField();
-      }
+    }
 
     /****************
     fieldDisplayRow
@@ -120,7 +225,7 @@ class RetirementField {
 
         myResponse = myResponse.replace("_FIELDNAME_", this.fieldName);
         myResponse = myResponse.replace("_FIELDVALUE_", this.fieldValue);
-        myResponse =  myResponse.replace("_FIELDDESCRIPTION_", this.fieldDescription);
+        myResponse = myResponse.replace("_FIELDDESCRIPTION_", this.fieldDescription);
 
         return myResponse;
 
