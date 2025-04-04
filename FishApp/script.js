abd9a752670b612d5b2923cde590cea280d9319c
alanhummer <62815820+alanhummer@users.noreply.github.com>
2025-04-03 19:37:22 -0500

    sdf
    sdf

--- FishApp/script.js
+++ FishApp/script.js
@@ -89,7 +89,7 @@ const weatherInfoDiv = document.getElementById('weather-info');
 const debugMessage = document.getElementById('debug');
 
 //Set the version in the status
-statusDiv.textContent = "v2025.04.02.01";
+statusDiv.textContent = "v2025.04.03.01";
 
 //Buttons
 const captureBtn = document.getElementById('captureBtn'); //Take Picture
