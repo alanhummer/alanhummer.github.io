c1ebd03384d671f7aba9c1559e406a27e6ef3d74
alanhummer <62815820+alanhummer@users.noreply.github.com>
2025-04-13 17:09:12 -0500

    sdf
    sdf

--- FishApp/cardgame.js
+++ FishApp/cardgame.js
@@ -14,19 +14,29 @@ function runTheCardGame() {
     document.getElementById('fishapp').style.display = "none";
     document.getElementById('cardapp').style.display = "block";
 
+    const urlParams = new URLSearchParams(window.location.search);
+    const myName = urlParams.get('name');
+    if (!myName) {
+        myName = "Nobody";
+    }
+
     //start web socket, add listeners for the events we want
-    const socket = new WebSocket('wss://i-saw-your-cards.deno.dev');
+    const socket = new WebSocket('wss://i-saw-your-cards.deno.dev?name=' + myName);
 
     //When it connects successfully, off we go
     socket.addEventListener('open', (event) => {
         console.log('Connected to server');
         document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>Connected to server" + "</li>";
-        socket.send('Hello Server!');
+        document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>**************</li>";
+        var sendMessage = 'Hello Server!';
+        socket.send(sendMessage);
+        document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>Client --> Server: " + sendMessage + "</li>";
+
 
         //Recieve message from server handler
         socket.addEventListener('message', (event) => {
             console.log('Message from server:', event.data);
-            document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>Server: " + event.data + "</li>";
+            document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>Server --> Client(me): " + event.data + "</li>";
         });
 
         //Send message to the server
@@ -34,14 +44,14 @@ function runTheCardGame() {
             cardappButtonReady = true;
             document.getElementById('cardapp-send').addEventListener('click', async () => {
                 socket.send(document.getElementById('cardapp-message').value);
-                document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>Client: " + document.getElementById('carapp-message').value + "</li>";
+                document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>Client --> Server: " + document.getElementById('carapp-message').value + "</li>";
             });
         }
-
 
         //When socket is closed handler
         socket.addEventListener('close', (event) => {
             console.log('Disconnected from server');
+            document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>**************</li>";
             document.getElementById('cardapp-log').innerHTML = document.getElementById('cardapp-log').innerHTML + "<li>Disconnected from server" + "</li>";
         });
 
