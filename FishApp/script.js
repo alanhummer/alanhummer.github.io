16ab1be4069c88d63b3600e979b94d24a79dee78
Alan Hummer <62815820+alanhummer@users.noreply.github.com>
2025-06-22 18:17:27 +0000

    Version Update

--- FishApp/script.js
+++ FishApp/script.js
@@ -81,7 +81,7 @@ const weatherInfoDiv = document.getElementById('weather-info');
 const debugMessage = document.getElementById('debug');
 
 //Set the version in the status
-statusDiv.textContent = "v2025.04.19.01";
+statusDiv.textContent = "v2025.06.22.01";
 
 //Buttons
 const captureBtn = document.getElementById('captureBtn'); //Take Picture
